89675a39f9df940bdb1200819f44c48bfdfe5409
João Emanoel <emanuel23pagani76@gmail.com>
2025-10-07 11:05:57 -0300

    a

--- mongodb/src/main/java/com/example/mongodb/controller/UsuarioController.java
+++ mongodb/src/main/java/com/example/mongodb/controller/UsuarioController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -27,6 +29,7 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @GetMapping
     public List<Usuario> ListTodos(){
         return usuarioService.ListTodos();
     }
